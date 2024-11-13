fbbc9d04720b27490cda8af53bb73617a138effc
231006 <109812774+adminouyang@users.noreply.github.com>
2024-11-14 00:49:47 +0800

    Update main.py

--- py/iptv源收集检测/main.py
+++ py/iptv源收集检测/main.py
@@ -588,8 +588,8 @@ all_lines_simple =  ["更新时间,#genre#"] +[version] +[about] +[daily_mtv]+ [
              ["💓优质个源,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪优质源.txt') + ['\n'] + \
              ["💓儿童专享,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪儿童专享.txt') + ['\n'] + \
              ["💓咪咕直播,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪咪咕直播.txt') + ['\n'] + \
-             #["🏀SPORTS⚽️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪sports.txt') + ['\n'] + \
-             #["🍹定制台☕️,#genre#"] + read_txt_to_array('py/iptv源收集检测/专区/♪定制源.txt') + ['\n'] + \
+             ["🏀SPORTS⚽️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪sports.txt') + ['\n'] + \
+             ["🍹定制台☕️,#genre#"] + read_txt_to_array('py/iptv源收集检测/专区/♪定制源.txt') + ['\n'] + \
              ["☘️湖南频道,#genre#"] + sort_data(hn_dictionary,set(correct_name_data(corrections_name,hn_lines))) + ['\n'] + \
              ["☘️湖北频道,#genre#"] + sort_data(hb_dictionary,set(correct_name_data(corrections_name,hb_lines))) + ['\n'] + \
              ["☘️广东频道,#genre#"] + sort_data(gd_dictionary,set(correct_name_data(corrections_name,gd_lines))) + ['\n'] + \
@@ -602,8 +602,8 @@ all_lines_simple =  ["更新时间,#genre#"] +[version] +[about] +[daily_mtv]+ [
 # ["奥运频道,#genre#"] + sort_data(Olympics_2024_Paris_dictionary,set(correct_name_data(corrections_name,Olympics_2024_Paris_lines))) + ['\n'] + \
 # 
 all_lines =  ["更新时间,#genre#"] +[version]  +[about] +[daily_mtv] + ['\n'] +\
-             #["💓专享源🅰️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪专享源①.txt') + ['\n'] + \
-             #["💓专享源🅱️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪专享源②.txt') + ['\n'] + \
+             ["💓专享源🅰️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪专享源①.txt') + ['\n'] + \
+             ["💓专享源🅱️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪专享源②.txt') + ['\n'] + \
              ["💓专享央视,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪优质央视.txt') + ['\n'] + \
              ["💓专享卫视,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪优质卫视.txt') + ['\n'] + \
              ["💓港澳台📶,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪港澳台.txt') + ['\n'] + \
@@ -612,8 +612,8 @@ all_lines =  ["更新时间,#genre#"] +[version]  +[about] +[daily_mtv] + ['\n']
              ["💓优质个源,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪优质源.txt') + ['\n'] + \
              ["💓儿童专享,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪儿童专享.txt') + ['\n'] + \
              ["💓咪咕直播,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪咪咕直播.txt') + ['\n'] + \
-             #["🏀SPORTS⚽️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪sports.txt') + ['\n'] + \
-             #["🍹定制台☕️,#genre#"] + read_txt_to_array('py/iptv源收集检测/专区/♪定制源.txt') + ['\n'] + \
+             ["🏀SPORTS⚽️,#genre#"] + read_txt_to_array('py/iptv源收集检测/主频道/♪sports.txt') + ['\n'] + \
+             ["🍹定制台☕️,#genre#"] + read_txt_to_array('py/iptv源收集检测/专区/♪定制源.txt') + ['\n'] + \
              ["🌐央视频道,#genre#"] + sort_data(ys_dictionary,set(correct_name_data(corrections_name,ys_lines))) + ['\n'] + \
              ["📡卫视频道,#genre#"] + sort_data(ws_dictionary,set(correct_name_data(corrections_name,ws_lines))) + ['\n'] + \
              ["上海频道,#genre#"] + sort_data(sh_dictionary,set(correct_name_data(corrections_name,sh_lines))) + ['\n'] + \
