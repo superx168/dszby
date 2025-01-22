c6466b539494fc9ff2ea5168aaf7f261172c0d65
231006 <109812774+adminouyang@users.noreply.github.com>
2025-01-22 19:13:57 +0800

    Create DIYP_config.py

---
+++ py/DIYP/DIYP_config.py
@@ -0,0 +1,63 @@
+
+ip_version_priority = "ipv4"
+
+source_urls = [
+    # "https://gh-proxy.com/https://raw.githubusercontent.com/frxz751113/IPTVzb1/refs/heads/main/%E7%BB%BC%E5%90%88%E6%BA%90.txt",
+    # "https://gh-proxy.com/https://raw.githubusercontent.com/dengmeiqing/IPTV1/refs/heads/main/live.txt"
+    "https://gh-proxy.com/https://raw.githubusercontent.com/250992941/iptv/refs/heads/main/st1.txt",
+    "https://gh-proxy.com/https://raw.githubusercontent.com/250992941/iptv/refs/heads/main/st2.txt",
+    "https://gh-proxy.com/https://raw.githubusercontent.com/l821706/IPTV/refs/heads/main/IPTV.txt",
+    # "https://live.zhoujie218.top/tv/iptv6.txt",
+    # "https://live.zhoujie218.top/tv/iptv4.txt",
+    # "https://www.mytvsuper.xyz/m3u/Yang.m3u",
+    # "https://tv.youdu.fan:666/live/",
+    # "http://ww.weidonglong.com/dsj.txt",
+    # "http://xhztv.top/zbc.txt",
+
+    # "https://ghp.ci/https://raw.githubusercontent.com/qingwen07/awesome-iptv/main/tvbox_live_all.txt",
+    # "https://ghp.ci/https://raw.githubusercontent.com/Guovin/TV/gd/output/result.txt",
+    # "http://home.jundie.top:81/Cat/tv/live.txt",
+    # "https://ghp.ci/https://raw.githubusercontent.com/vbskycn/iptv/master/tv/hd.txt",
+    # "https://cdn.jsdelivr.net/gh/YueChan/live@main/IPTV.m3u",
+    # "https://fm1077.serv00.net/SmartTV.m3u",
+    # "https://ghp.ci/https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt"
+]
+
+url_blacklist = [
+    "http",
+    "https",
+
+]
+url_whitelist = [
+    "p2p",
+    "P2p",
+    "p3p",
+    "mitv",
+    "z.b.bkpcp.top",
+    "http://cc-ynbit-wszhibo.ifengli.com:2000",
+]
+
+announcements = [
+    # {
+    #     "channel": "公告",
+    #     "entries": [
+    #         {"name": "请阅读", "url": "https://liuliuliu.tv/api/channels/1997/stream",
+    #          "logo": "http://175.178.251.183:6689/LR.jpg"},
+    #         {"name": "yuanzl77.github.io", "url": "https://liuliuliu.tv/api/channels/233/stream",
+    #          "logo": "http://175.178.251.183:6689/LR.jpg"},
+    #         {"name": "更新日期", "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E4%B8%BB%E8%A7%92.mp4",
+    #          "logo": "http://175.178.251.183:6689/LR.jpg"},
+    #         {"name": None, "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E8%B5%B7%E9%A3%8E%E4%BA%86.mp4",
+    #          "logo": "http://175.178.251.183:6689/LR.jpg"}
+    #     ]
+    # }
+]
+
+epg_urls = [
+    "https://live.fanmingming.com/e.xml",
+    "http://epg.51zmt.top:8000/e.xml",
+    "http://epg.aptvapp.com/xml",
+    "https://epg.pw/xmltv/epg_CN.xml",
+    "https://epg.pw/xmltv/epg_HK.xml",
+    "https://epg.pw/xmltv/epg_TW.xml"
+]
