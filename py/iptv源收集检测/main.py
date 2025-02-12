6eb46e2fdd1f3f26a0cc1d58be5b68a026fda5d6
231006 <109812774+adminouyang@users.noreply.github.com>
2025-02-12 09:41:40 +0800

    Update main.py

--- py/iptv源收集检测/main.py
+++ py/iptv源收集检测/main.py
@@ -575,7 +575,7 @@ beijing_time = utc_time + timedelta(hours=8)
 # 格式化为所需的格式
 formatted_time = beijing_time.strftime("%Y%m%d %H:%M:%S")
 
-about_video1="https://gitee.com/kabigo/tv/raw/master/assets/about1080p.mp4"
+about_video1="https://v.cdnlz12.com/20250131/18183_a5e8965b/index.m3u8"
 #about_video2="https://gitlab.com/p2v5/wangtv/-/raw/main/about1080p.mp4"
 version=formatted_time+","+about_video1
 #about="关于本源,"+about_video2
