ad1767178b5f3ba903203e5b20536a246e3eec09
231006 <109812774+adminouyang@users.noreply.github.com>
2025-02-14 23:27:38 +0800

    Create config.py

---
+++ py/iptv源收集检测/主频道/专享频道/py/组播/config.py
@@ -0,0 +1,35 @@
+ip_version_priority = "ipv6"
+
+source_urls = [
+    "https://ghproxy.cc/https://raw.githubusercontent.com/lalifeier/IPTV/refs/heads/main/txt/udpxy/全国.txt",
+
+]
+
+url_blacklist = [
+  
+]
+url_whitelist = [
+    "rtp",
+    "udp",
+]
+
+announcements = [
+    # {
+    #     "channel": "公告",
+    #     "entries": [
+    #         {"name": "请阅读", "url": "https://liuliuliu.tv/api/channels/1997/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
+    #         {"name": "yuanzl77.github.io", "url": "https://liuliuliu.tv/api/channels/233/stream", "logo": "http://175.178.251.183:6689/LR.jpg"},
+    #         {"name": "更新日期", "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E4%B8%BB%E8%A7%92.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"},
+    #         {"name": None, "url": "https://gitlab.com/lr77/IPTV/-/raw/main/%E8%B5%B7%E9%A3%8E%E4%BA%86.mp4", "logo": "http://175.178.251.183:6689/LR.jpg"}
+    #     ]
+    # }
+]
+
+epg_urls = [
+    "https://live.fanmingming.com/e.xml",
+    "http://epg.51zmt.top:8000/e.xml",
+    "http://epg.aptvapp.com/xml",
+    "https://epg.pw/xmltv/epg_CN.xml",
+    "https://epg.pw/xmltv/epg_HK.xml",
+    "https://epg.pw/xmltv/epg_TW.xml"
+]
