d5f67a714edd2484d3e2a619f68af3f683b45a94
231006 <109812774+adminouyang@users.noreply.github.com>
2025-02-21 21:38:49 +0800

    Update zubo.sh

--- py/iptv源收集检测/主频道/专享频道/py/组播/zubo.sh
+++ py/iptv源收集检测/主频道/专享频道/py/组播/zubo.sh
@@ -17,26 +17,73 @@ fi
 # 根据用户选择设置城市和相应的stream
 case $city_choice in
     1)
-        city="py/iptv源收集检测/主频道/专享频道/py/组播/template/Beijing_liantong_145"
+        city="Zhejiang_120"
+        stream="udp/233.50.201.63:5140"
+        channel_key="浙江电信"
+        ;;
+    2)
+        city="Jiangsu"
+        stream="udp/239.49.8.19:9614"
+        channel_key="江苏电信"
+        ;;
+    3)
+        city="Shanghai_103"
+        stream="udp/239.45.1.42:5140"
+	channel_key="上海电信"
+        ;;
+    4)
+        city="Beijing_liantong_145"
         stream="rtp/239.3.1.249:8001"
         channel_key="北京联通"
         ;;
-
-    2)
-        city="py/iptv源收集检测/主频道/专享频道/py/组播/template/Sichuan_333"
+    5)
+        city="Hubei_90"
+        stream="rtp/239.69.1.249:11136"
+        channel_key="湖北电信"
+        ;;
+    6)
+        city="Sichuan_333"
         stream="udp/239.93.42.33:5140"
         channel_key="四川电信"
         ;;
-
-    3)
-        city="py/iptv源收集检测/主频道/专享频道/py/组播/template/Anhui_191"
+    7)
+        city="Shanxi_184"
+        stream="rtp/226.0.2.152:9128"
+        channel_key="山西联通"
+        ;;
+    8)
+        city="Guangxi"
+        stream="udp/239.81.0.107:4056"
+        channel_key="广西电信"
+        ;;
+    9)
+        city="Shanxi_117"
+        stream="udp/239.1.1.7:8007"
+        channel_key="山西电信"
+        ;;
+    10)
+        city="Tianjin_160"
+        stream="udp/225.1.2.190:5002"
+        channel_key="天津联通"
+        ;;
+    11)
+        city="Chongqing_77"
+        stream="udp/225.0.4.188:7980"
+        channel_key="重庆联通"
+	;;
+    12)
+        city="Anhui_191"
         stream="rtp/238.1.78.137:6968"
         channel_key="安徽电信"
 	;;
-
+    13)
+        city="Chongqing_161"
+        stream="rtp/235.254.196.249:1268"
+        channel_key="重庆电信"
+	;;
     0)
         # 如果选择是“全部选项”，则逐个处理每个选项
-        for option in {1..3}; do
+        for option in {1..13}; do
           bash "$0" $option  # 假定fofa.sh是当前脚本的文件名，$option将递归调用
         done
         exit 0
