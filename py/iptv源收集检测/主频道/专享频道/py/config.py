36834a0d253e0e682831e7e8d13e729c6e66eb20
231006 <109812774+adminouyang@users.noreply.github.com>
2025-02-24 10:53:27 +0800

    Update config.py

--- py/iptv源收集检测/主频道/专享频道/py/config.py
+++ py/iptv源收集检测/主频道/专享频道/py/config.py
@@ -1,7 +1,12 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
-    "https://ghproxy.cc/https://raw.githubusercontent.com/lalifeier/IPTV/refs/heads/main/txt/udpxy/全国.txt",
+    "https://ghproxy.cc/https://raw.githubusercontent.com/lalifeier/IPTV/refs/heads/main/txt/udpxy/天津.txt",  #联通
+    "https://ghproxy.cc/https://raw.githubusercontent.com/lalifeier/IPTV/refs/heads/main/txt/udpxy/山西.txt",  #联通
+    "https://ghproxy.cc/https://raw.githubusercontent.com/lalifeier/IPTV/refs/heads/main/txt/udpxy/黑龙江.txt",  #联通
+    "https://ghproxy.cc/https://raw.githubusercontent.com/lalifeier/IPTV/refs/heads/main/txt/udpxy/重庆.txt",  #联通
+    "https://ghproxy.cc/https://raw.githubusercontent.com/mlzlzj/iptv/refs/heads/main/iptv_list.txt",  #湖南电信
+    "https://ghproxy.cc/https://raw.githubusercontent.com/adminouyang/231006/refs/heads/main/py/安徽组播/iptv_list.txt"  #电信
 
 ]
 
