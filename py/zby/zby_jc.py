760c9774d5b3b3a39e5149e4dcf8fa4eb09c49f2
231006 <109812774+adminouyang@users.noreply.github.com>
2025-03-04 10:43:38 +0800

    Update zby_jc.py

--- py/zby/zby_jc.py
+++ py/zby/zby_jc.py
@@ -70,7 +70,7 @@ merge_txt_files(urls)
 converter = OpenCC('t2s.json')#繁转简
 #converter = OpenCC('s2t.json')#简转繁
 # 打开txt文件
-with open('汇总.txt', 'r', encoding='utf-8') as file:
+with open('py/zby/汇总.txt', 'r', encoding='utf-8') as file:
     traditional_text = file.read()
 # 进行繁体字转简体字的转换
 simplified_text = converter.convert(traditional_text)
