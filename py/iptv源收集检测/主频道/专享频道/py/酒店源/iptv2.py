e806861c350b5a5778f337b10e457cf3d4007e72
231006 <109812774+adminouyang@users.noreply.github.com>
2025-03-13 20:56:37 +0800

    Update iptv2.py

--- py/iptv源收集检测/主频道/专享频道/py/酒店源/iptv2.py
+++ py/iptv源收集检测/主频道/专享频道/py/酒店源/iptv2.py
@@ -42,7 +42,7 @@ for file_name in os.listdir(folder_path):
     if file_name.endswith('.txt'):
         file_paths = os.path.join(folder_path, file_name)
         print(file_paths)
-    with open('file_paths', 'r', encoding='utf-8') as file:
+    with open(file_paths, 'r', encoding='utf-8') as file:
             lines = file.readlines()
             for line in lines:
                 url = line.strip()
