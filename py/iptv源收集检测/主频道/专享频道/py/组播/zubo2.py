57fdf700df6c562039f4d8cdad1591df31f3942b
231006 <109812774+adminouyang@users.noreply.github.com>
2025-05-19 14:20:27 +0800

    Create zubo2.py

---
+++ py/iptv源收集检测/主频道/专享频道/py/组播/zubo2.py
@@ -0,0 +1,15 @@
+from zubo import multicast_province
+config_files = ["py/iptv源收集检测/主频道/专享频道/py/组播/ip/江苏电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/上海电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/浙江电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/江西电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/安徽电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/四川电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/贵州电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/重庆电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/陕西电信_config.txt", 
+                "py/iptv源收集检测/主频道/专享频道/py/组播/ip/宁夏电信_config.txt"
+               ]
+for config_file in config_files:
+    multicast_province(config_file)
+print(f"组播地址获取完成")
