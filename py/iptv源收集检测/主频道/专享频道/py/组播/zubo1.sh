cd7a4b9e7a48a8ab798fff06a13f0fb7c6fd08b3
231006 <109812774+adminouyang@users.noreply.github.com>
2025-06-23 13:17:16 +0800

    Update zubo1.sh

--- py/iptv源收集检测/主频道/专享频道/py/组播/zubo1.sh
+++ py/iptv源收集检测/主频道/专享频道/py/组播/zubo1.sh
@@ -135,13 +135,14 @@ ip3=$(awk 'NR==3{print $1}' $result_ip)
 # rm -f speedtest_${city}_$time.log  
 # 将最快的3个IP保存到配置文件中
 # echo "保存最快的3个IP到 ${city}_config.txt"
-echo "#${city} 最快的3个IP" > py/iptv源收集检测/主频道/专享频道/py/组播/ip/${city}_config.txt
-echo "$ip1" >> py/iptv源收集检测/主频道/专享频道/py/组播/ip/${city}_config.txt
-echo "$ip2" >> py/iptv源收集检测/主频道/专享频道/py/组播/ip/${city}_config.txt
-echo "$ip3" >> py/iptv源收集检测/主频道/专享频道/py/组播/ip/${city}_config.txt
+config_file="py/iptv源收集检测/主频道/专享频道/py/组播/ip/${city}_config.txt"
+echo "#${city} 最快的3个IP" > config_file
+echo "$ip1" >> config_file
+echo "$ip2" >> config_file
+echo "$ip3" >> config_file
 
 # 清理临时文件$result_ip 
-rm -f speedtest_${city}_$time.log 
+rm -f speedtest_${city}_$time.log $result_ip 
 echo "${city}_config.txt 测试完成，生成可用文件：'py/iptv源收集检测/主频道/专享频道/py/组播/ip/${city}_config.txt'"
 
 
